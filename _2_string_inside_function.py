f8691a8cf4987e7d7894343058b0a8a82c0884df
dhanshri <112890281+dhanshri888@users.noreply.github.com>
2022-09-25 14:59:08 +0530

    assihnment 3
    que no 2 - string inside the function

---
+++ _2_string_inside_function.py
@@ -0,0 +1,24 @@
+# string inside function 
+
+# write a python function to reverse a string
+
+# sample string : "1234abcd"
+
+# excepted output : "dcba4321"
+
+
+def my_function (x):
+
+    return x[::-1]
+
+sub = my_function("1234abcd")
+
+print(sub)
+
+    
+
+
+# substring = "1234abcd"[::-1]
+# print(substring)
+
+
